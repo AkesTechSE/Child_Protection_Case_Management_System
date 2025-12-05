5dcd8d482a3dcbb2a0d669ddae7d6a3c425aca3b
Aklilu Abera <akililuabera44@gmail.com>
2025-12-05 12:15:20 +0300

    first update

--- child-case-management/src/App.js
+++ child-case-management/src/App.js
@@ -1,14 +1,16 @@
-
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import { AuthProvider } from './context/AuthContext';
+
 import MainLayout from './components/layout/MainLayout';
 import AuthLayout from './components/layout/AuthLayout';
+
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import RegisterCasePage from './pages/Cases/RegisterCasePage';
+
 import ProtectedRoute from './components/auth/ProtectedRoute';
 
 function App() {
@@ -16,34 +18,42 @@ function App() {
     <AppProvider>
       <AuthProvider>
         <Routes>
-          {/* Public Routes */}
+          {/* Public Route */}
           <Route path="/" element={<LandingPage />} />
-          
-          {/* Auth Routes */}
-          <Route path="/login" element={
-            <AuthLayout>
-              <LoginPage />
-            </AuthLayout>
-          } />
-          
-          {/* Protected Routes */}
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <MainLayout>
-                <DashboardPage />
-              </MainLayout>
-            </ProtectedRoute>
-          } />
-          
-          <Route path="/cases/register" element={
-            <ProtectedRoute>
-              <MainLayout>
-                <RegisterCasePage />
-              </MainLayout>
-            </ProtectedRoute>
-          } />
-          
-          {/* Add more routes as needed */}
+
+          {/* Auth Route */}
+          <Route
+            path="/login"
+            element={
+              <AuthLayout>
+                <LoginPage />
+              </AuthLayout>
+            }
+          />
+
+          {/* Protected Dashboard */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <MainLayout>
+                  <DashboardPage />
+                </MainLayout>
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Register Case */}
+          <Route
+            path="/cases/register"
+            element={
+              <ProtectedRoute>
+                <MainLayout>
+                  <RegisterCasePage />
+                </MainLayout>
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </AuthProvider>
     </AppProvider>
