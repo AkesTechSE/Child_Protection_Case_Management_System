52230b9dc439e97f9c8832fa50271dfac7c02c1d
chalasimon <chally.r@hotmail.com>
2025-12-18 14:36:55 +0300

    fix: production env created

---
+++ resources/views/app.blade.php
@@ -0,0 +1,27 @@
+@php
+    $manifest = json_decode(
+        file_get_contents(public_path('build/.vite/manifest.json')),
+        true
+    );
+
+    $entry = $manifest['index.html'];
+@endphp
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>App</title>
+
+    @if(isset($entry['css']))
+        @foreach($entry['css'] as $css)
+            <link rel="stylesheet" href="{{ asset('build/' . $css) }}">
+        @endforeach
+    @endif
+</head>
+<body>
+    <div id="root"></div>
+
+    <script type="module" src="{{ asset('build/' . $entry['file']) }}"></script>
+</body>
+</html>
