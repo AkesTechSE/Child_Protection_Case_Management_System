315e6953221f7c944b48e23c681038bfb201077a
chalasimon <chally.r@hotmail.com>
2025-12-21 01:11:53 +0300

    fix: api url updated

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const DEFAULT_API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
+const DEFAULT_API_BASE_URL = 'https://https://perpetrators.et/api' || 'http://localhost:8000/api'
 
 const api = axios.create({
   baseURL: DEFAULT_API_BASE_URL,
